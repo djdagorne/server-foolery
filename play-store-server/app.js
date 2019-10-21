73bacea4a272d74aaceb45c018a6c2d8a1af12b2
Dexter Dagorne <dj.dagorne@gmail.com>
2019-10-21 16:47:26 -0400

    fixed sorting algorithm

--- play-store-server/app.js
+++ play-store-server/app.js
@@ -11,7 +11,7 @@ app.use(morgan('common'));
 const applist = require('./appList.js');
 
 app.get('/apps', (req, res) => {
-    const { genres, sort = "" } = req.query;
+    const { genres, sort } = req.query;
 
     if(!genres){
         return res.status(400).send('you need to pick a genre')
@@ -19,9 +19,15 @@ app.get('/apps', (req, res) => {
 
     const genreFilteredApps = applist.filter(filtered => 
         filtered.Genres.toLowerCase().includes(genres.toLowerCase()));
-
-    if (sort) {
+    
+    if (sort === "App") {
         genreFilteredApps.sort((a, b) => {
+            console.log(a.Rating, ' ', b.Rating)
+            return a[sort].toLowerCase() > b[sort].toLowerCase() ? 1 : a[sort].toLowerCase() < b[sort].toLowerCase() ? -1 : 0; 
+        })
+    }if (sort === "Rating"){
+        genreFilteredApps.sort((a, b) => {
+            console.log(a.Rating, ' ', b.Rating)
             return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0; 
         })
     }
